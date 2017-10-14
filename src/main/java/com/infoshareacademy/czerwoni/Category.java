dfb1e4baa0afc539982f084d57e95dc4b33edd0e
Marcin Pastwa <marcinpastwa@wp.pl>
2017-10-14 10:21:09 +0200

    question added

--- src/main/java/com/infoshareacademy/czerwoni/Category.java
+++ src/main/java/com/infoshareacademy/czerwoni/Category.java
@@ -1,4 +1,49 @@
 package com.infoshareacademy.czerwoni;
 
 public class Category {
+    private int categoryId;
+    private int categoryParent;
+    private int categoryPosition;
+    private boolean categoryVisible;
+    private String categoryName;
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getCategoryParent() {
+        return categoryParent;
+    }
+
+    public void setCategoryParent(int categoryParent) {
+        this.categoryParent = categoryParent;
+    }
+
+    public int getCategoryPosition() {
+        return categoryPosition;
+    }
+
+    public void setCategoryPosition(int categoryPosition) {
+        this.categoryPosition = categoryPosition;
+    }
+
+    public boolean isCategoryVisible() {
+        return categoryVisible;
+    }
+
+    public void setCategoryVisible(boolean categoryVisible) {
+        this.categoryVisible = categoryVisible;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
--- src/main/java/com/infoshareacademy/czerwoni/ParseXML.java
+++ src/main/java/com/infoshareacademy/czerwoni/ParseXML.java
@@ -1,55 +1,63 @@
 package com.infoshareacademy.czerwoni;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ParseXML {
 
-    /* TODO
-    1.Tworzenie pytań i zapis do pliku. Jeżeli plik nie istnieje to program powinien go stworzyć.
-    2.Odczyt pytania
-    3.Przypisanie pytań do kategorii
-    4.
-    */
-
-
-
     public static void main(String args[]) {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         try {
 
-            File fXmlFile = new File("CatQuestions.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            File fXmlFile = new File("Categories.xml");
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(fXmlFile);
-            doc.getDocumentElement().normalize();
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-            System.out.println("Root element :" + doc.getElementsByTagName("ELEKTRONIKA").item(0));
-            NodeList nList = doc.getElementsByTagName("FOTOGRAFIA");
+            NodeList questList = doc.getElementsByTagName("quest-item");
+            NodeList catList = doc.getElementsByTagName("cat-item");
+            for (int k=0;k<questList.getLength();k++) {
+                Node questItem = questList.item(k);
+                Element quest = (Element) questItem;
+                System.out.println(quest.getTextContent());
+                for (int i = 0; i < catList.getLength(); i++) {
+                    Node catItem = catList.item(i);
+                    if (catItem.getNodeType() == Node.ELEMENT_NODE) {
+                        Element category = (Element) catItem;
+                        NodeList itemList = category.getChildNodes();
+                        for (int j = 0; j < itemList.getLength(); j++) {
+                            Node details = itemList.item(j);
+                            if (details.getNodeType() == Node.ELEMENT_NODE) {
+                                Element itemName = (Element) details;
+                                //System.out.println(itemName.getTagName()+" : "+itemName.getTextContent());
 
+                                if(quest.getAttribute("id").equals(itemName.getAttribute("question"))) {
 
-            for(int i=0; i<nList.getLength();i++){
-                Node node = nList.item(i);
-                Element question = (Element) node;
-                System.out.println("Element: "+node.getNodeName());
-                System.out.println(i);
-                System.out.println(node.getAttributes().getNamedItem("pytanie"));
-                System.out.println("Odpowiedz na pytanie: "+question.getAttribute("pytanie"));
+                                        System.out.println(itemName.getTextContent());
+
+                                }
+
+                            }
+                        }
+                    }
+                }
             }
 
-
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-
+        catch (ParserConfigurationException e){
+            e.printStackTrace();
+        }
+        catch (SAXException e){
+            e.printStackTrace();
+        }
     }
-
 
 }
 
