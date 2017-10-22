7327fc4e3e25c22b203b8f0db744b911021b79e8
bmkuczma <bm.kuczma@gmail.com>
2017-10-22 20:17:29 +0200

    Rozpoczecie wyszukiwarki po frazie, wczytuje kategorie

---
+++ src/main/java/com/infoshareacademy/czerwoni/PhraseFinder.java
@@ -0,0 +1,30 @@
+package com.infoshareacademy.czerwoni;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PhraseFinder {
+
+    public String PhraseScanner(String s) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String phrase = scanner.nextLine();
+        System.out.println(phrase);
+
+
+        return phrase;
+    }
+
+    public void PhraseResearch(String s, ArrayList<AllegroCategory> allegroCategories) {
+
+        String search = s;
+        for (AllegroCategory allegroCategory : allegroCategories) {
+            if (allegroCategory.getCatName().contains(search)) {
+                System.out.println(allegroCategory.getCatName());
+            } else {
+                System.out.println("Shit happens. :0");
+            }
+        }
+    }
+}
