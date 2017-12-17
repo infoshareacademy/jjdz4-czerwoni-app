aba6abec098f756aa9318d630bdb081e7f0d155e
MMil05 <maksymilian.milkowski@gmail.com>
2017-12-17 20:27:07 +0100

    JZ4CS-47_testy test classes added

---
+++ konsola/src/test/java/com/infoshareacademy/czerwoni/product/BarCodeReaderTest.java
@@ -0,0 +1,11 @@
+package com.infoshareacademy.czerwoni.product;
+
+import org.junit.Test;
+
+public class BarCodeReaderTest {
+
+    @Test
+    public void decodeBarcodeFromFileTest() {
+        // // stworzyc kilka proc.testowych - gdy podano a) pusty filename b)null c) nieistniejacy d)poprawny
+    }
+}
---
+++ konsola/src/test/java/com/infoshareacademy/czerwoni/product/ProductProcessorTest.java
@@ -0,0 +1,11 @@
+package com.infoshareacademy.czerwoni.product;
+
+import org.junit.Test;
+
+public class ProductProcessorTest {
+
+    @Test
+    public void getProductDataFromAPITest() {
+        // stworzyc kilka proc.testowych - gdy podano a) pusty barcode b)null c) inny niz ciag cyfr d)poprawny
+    }
+}
