680cd672d48aa593893d65b017bfbadae312aab8
MMil05 <maksymilian.milkowski@gmail.com>
2017-12-22 13:28:35 +0100

    JZ4CS-47_testy BarCodeReader class tests

--- konsola/src/test/java/com/infoshareacademy/czerwoni/product/BarCodeReaderTest.java
+++ konsola/src/test/java/com/infoshareacademy/czerwoni/product/BarCodeReaderTest.java
@@ -4,8 +4,25 @@ import org.junit.Test;
 
 public class BarCodeReaderTest {
 
+    private static final String PRINT_MSG = "Odczytany kod kreskowy: ";
+
     @Test
-    public void decodeBarcodeFromFileTest() {
-        // // stworzyc kilka proc.testowych - gdy podano a) pusty filename b)null c) nieistniejacy d)poprawny
+    public void decodeBarcodeFromFile_FileExists() {
+        System.out.println(PRINT_MSG + BarCodeReader.decodeBarcodeFromFile("/home/mm/kod.png"));
+    }
+
+    @Test
+    public void decodeBarcodeFromFile_FileNotExists() {
+        System.out.println(PRINT_MSG + BarCodeReader.decodeBarcodeFromFile("/home/abc/xyz.png"));
+    }
+
+    @Test
+    public void decodeBarcodeFromFile_EmptyArg() {
+        System.out.println(PRINT_MSG + BarCodeReader.decodeBarcodeFromFile(""));
+    }
+
+    @Test
+    public void decodeBarcodeFromFile_NullArg() {
+        System.out.println(PRINT_MSG + BarCodeReader.decodeBarcodeFromFile(null));
     }
 }
