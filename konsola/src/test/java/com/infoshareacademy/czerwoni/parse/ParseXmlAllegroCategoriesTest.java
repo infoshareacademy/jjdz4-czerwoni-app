a7331cfebc37a16d174b5d43c8306f83ed76db20
michalklawik <WejhiT261f>
2018-01-22 21:43:00 +0100

    Written tests for link generating method

--- konsola/src/test/java/com/infoshareacademy/czerwoni/parse/ParseXmlAllegroCategoriesTest.java
+++ konsola/src/test/java/com/infoshareacademy/czerwoni/parse/ParseXmlAllegroCategoriesTest.java
@@ -1,17 +1,5 @@
 package com.infoshareacademy.czerwoni.parse;
 
-import org.junit.Test;
-
 public class ParseXmlAllegroCategoriesTest {
-
-    @Test
-    public void deserialization_FileDoesNotExist() {
-
-    }
-
-    @Test
-    public void deserialization_FileDoesExist() {
-
-    }
 
 }
