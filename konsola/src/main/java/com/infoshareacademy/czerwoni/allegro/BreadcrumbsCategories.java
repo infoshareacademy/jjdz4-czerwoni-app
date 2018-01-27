af19c09e4800b208e44fbf0efb24f53d3b1d129f
bmkuczma <bm.kuczma@gmail.com>
2018-01-27 11:49:09 +0100

    Refactor unused classes, variables and comments

--- konsola/src/main/java/com/infoshareacademy/czerwoni/allegro/BreadcrumbsCategories.java
+++ konsola/src/main/java/com/infoshareacademy/czerwoni/allegro/BreadcrumbsCategories.java
@@ -1,29 +1,22 @@
 package com.infoshareacademy.czerwoni.allegro;
 
-import com.infoshareacademy.czerwoni.allegro.AllegroCategory;
 import com.infoshareacademy.czerwoni.parse.ParseXmlAllegroCategories;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class BreadcrumbsCategories {
 
     public static void breadcrumbsPrinter(int idAtm){
 
-    /**
-     Klasa do wypisywania parentów np. nokia wypisze:
-     Telefony i Akcesoria < -- Telefony komórkowe < - - Nokia
-     ***/
+
 
         List<AllegroCategory> allegroCategories = ParseXmlAllegroCategories.deserialization();
         StringBuilder sB = new StringBuilder();
         int idHelper = idAtm;
-        //System.out.println("wchodzi do petli alC"+ idAtm);
 
         for (AllegroCategory alC : allegroCategories) {
             if (alC.getCatId().equals(idAtm)) {
                 sB.append(alC.getCatName());
-              //  System.out.println("pierwszy if append");
                 idHelper = alC.getCatParent();
             }
             if (alC.getCatId().equals(0)) {
