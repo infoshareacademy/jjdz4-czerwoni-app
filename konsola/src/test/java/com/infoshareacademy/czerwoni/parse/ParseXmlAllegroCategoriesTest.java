f4230b38d457e80ba0c12ea05013c15d0560549a
michalklawik <WejhiT261f>
2018-01-27 19:03:55 +0100

    added tests for Parser

--- konsola/src/test/java/com/infoshareacademy/czerwoni/parse/ParseXmlAllegroCategoriesTest.java
+++ konsola/src/test/java/com/infoshareacademy/czerwoni/parse/ParseXmlAllegroCategoriesTest.java
@@ -1,5 +1,22 @@
 package com.infoshareacademy.czerwoni.parse;
 
+import com.infoshareacademy.czerwoni.allegro.AllegroCategory;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 public class ParseXmlAllegroCategoriesTest {
 
+    @Test
+    public void parseXml_ListNotEmpty() {
+        List<AllegroCategory> categories = ParseXmlAllegroCategories.deserialization();
+        assertThat(!categories.isEmpty());
+    }
+
+    @Test
+    public void parseXml_ContainsCorrectData() {
+        List<AllegroCategory> categories = ParseXmlAllegroCategories.deserialization();
+        assertThat(categories.stream().filter(category -> category.getCatId()==5).findFirst().get().getCatId() == 5);
+    }
 }
